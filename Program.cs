8790f8ae2bc38539e3e045073dbc434e91d5b6c7
Nichole Axel <nfrancis22@gmail.com>
2022-06-08 16:07:53 -0400

    more api points

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Services;
 using Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<TodoDbSettings>(builder.Configuration.GetSection("TodoDB"));
 builder.Services.AddSingleton<ITodoItemService, TodoItemService>();
 var app = builder.Build();
 
-app.MapGet("/todo-items", () => {});
+app.MapGet("/todo-items", async (ITodoItemService todoItemService) => {
+    var todos = await todoItemService.GetAllTodoItems();
+    return Results.Ok(todos);
+});
 
 app.MapGet("/todo-items/{id}", async (string id, ITodoItemService todoItemService) => {
     var todoItem = await todoItemService.GetTodoItemById(id);
@@ -15,7 +19,6 @@ app.MapGet("/todo-items/{id}", async (string id, ITodoItemService todoItemServic
 app.MapPost("/todo-items", () => {});
 app.MapPut("/todo-items/{id}", (string id) => {}); //to update need id
 app.MapDelete("/todo-items/{id}", (string id) => {}); //to delete need id
-
 
 app.Run();
 
