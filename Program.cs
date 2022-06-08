72e385b62592b651653ae850c83fe2593ece452e
Nichole Axel <nfrancis22@gmail.com>
2022-06-08 16:32:01 -0400

    fixed points

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using Services;
 using Settings;
+using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<TodoDbSettings>(builder.Configuration.GetSection("TodoDB"));
 builder.Services.AddSingleton<ITodoItemService, TodoItemService>();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/todo-items", async (ITodoItemService todoItemService) => {
     var todos = await todoItemService.GetAllTodoItems();
@@ -18,7 +24,7 @@ app.MapGet("/todo-items/{id}", async (string id, ITodoItemService todoItemServic
 
 app.MapPost("/todo-items", async (TodoItem newVoodo, ITodoItemService service) => {
     TodoItem createdTodoItem = await service.CreateTodoItem(newVoodo);
-    return Results.Created($"todo-items/{createdTodoItem.Id}"); //if you follow this link you will get
+    return Results.CreatedAtRoute($"todo-items/{createdTodoItem.Id}"); //if you follow this link you will get
     //to this new item
 });
 app.MapPut("/todo-items/{id}", (string id) => {}); //to update need id
--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -1,38 +1,42 @@
+using Microsoft.Extensions.Options;
 using Models;
+using MongoDB.Driver;
 using Settings;
-namespace Services;
 
+namespace Services;
 
 public interface ITodoItemService
 {
-    Task<IEnumerable<TodoItemService>> GetAllTodoItems();
+    Task<IEnumerable<TodoItem>> GetAllTodoItems(); 
     Task<TodoItem> GetTodoItemById(string id);
+
     Task<TodoItem> UpdateTodoItem(string id, TodoItem todoItem);
 
-    Task<TodoItem> CreateTodoItem(TodoItem todoItem);
+    Task<TodoItem> CreateTodoItem(TodoItem newTodoItem);
 }
-public class TodoItemService : ITodoItemService
 
+public class TodoItemService : ITodoItemService
 {
     private readonly IMongoCollection<TodoItem> todoItemCollection;
-    public TodoItemService(IOptions<TodoDbSettings> todoDbSettings);
+    public TodoItemService(IOptions<TodoDbSettings> todoDbSettings)
     {
         var client = new MongoClient(todoDbSettings.Value.ConnectionString);
-        var db = client.GetDatabase(TodoDbSettings.Value.DatabaseName);
-        todoItemCollection = db.GetCollection<TodoItem>(todoDbSettings.Value.DatabaseName);
+        var db = client.GetDatabase(todoDbSettings.Value.DatabaseName);
+        todoItemCollection = db.GetCollection<TodoItem>(todoDbSettings.Value.CollectionName);
+        
     }
     public async Task<TodoItem> CreateTodoItem(TodoItem newTodoItem)
     {
         await todoItemCollection.InsertOneAsync(newTodoItem);
+
         return newTodoItem;
-        
+       
     }
 
     public async Task<IEnumerable<TodoItem>> GetAllTodoItems()
-
     {
-        var res = await todoItemCollection.FindAsync( x =>)
-        throw new NotImplementedException();
+        var res = await todoItemCollection.FindAsync(x => true);
+        return res.ToList();
     }
 
     public Task<TodoItem> GetTodoItemById(string id)
